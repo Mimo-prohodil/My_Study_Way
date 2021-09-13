8b2ba056d8140d6454b37f4565e0ee3b170c7353
Mimo-prohodil <aka47beta@yandex.ru>
2021-09-13 09:52:53 +0300

    111

--- Blackjack/GenericPlayer.cpp
+++ Blackjack/GenericPlayer.cpp
@@ -1,1 +1,11 @@
 #include "GenericPlayer.h"
+#include <iostream>
+
+inline bool GenericPlayer::IsBusted(){
+	return GetValue() > 21;
+}
+inline void GenericPlayer::Bust(){
+	if (IsBusted()) {
+		std::cout << "Player: " << m_Name << " has bust." << std::endl;
+	}
+}
--- Blackjack/Player.cpp
+++ Blackjack/Player.cpp
@@ -1,1 +1,13 @@
 #include "Player.h"
+bool Player::IsHitting() override const {
+
+}
+void Player::Win() const {
+
+}
+void Player::Lose() const {
+
+}
+void Player::Push() const {
+
+}
