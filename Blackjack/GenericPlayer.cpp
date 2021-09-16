cc5773716e3c34580570b98bda3bf8deb46f6992
Mimo-prohodil <aka47beta@yandex.ru>
2021-09-16 12:36:02 +0300

    upt

--- Blackjack/GenericPlayer.cpp
+++ Blackjack/GenericPlayer.cpp
@@ -1,11 +1,42 @@
 #include "GenericPlayer.h"
 #include <iostream>
 
-inline bool GenericPlayer::IsBusted(){
-	return GetValue() > 21;
+GenericPlayer::GenericPlayer(const std::string& name) : m_Name(name){}
+
+inline bool GenericPlayer::IsBusted() const{
+	return GetTotal() > 21;
 }
-inline void GenericPlayer::Bust(){
+inline void GenericPlayer::Bust() const{
 	if (IsBusted()) {
-		std::cout << "Player: " << m_Name << " has bust." << std::endl;
+		std::cout << "Player: " << m_Name << " busts." << std::endl;
 	}
 }
+std::ostream& operator<<(std::ostream& os, const GenericPlayer& aGenericPlayer)
+{
+    os << aGenericPlayer.m_Name << ":\t";
+
+    std::vector<Card*>::const_iterator pCard;
+    if (!aGenericPlayer.m_Cards.empty())
+    {
+        for (pCard = aGenericPlayer.m_Cards.begin();
+            pCard != aGenericPlayer.m_Cards.end();
+            ++pCard)
+        {
+            os << *(*pCard) << "\t";
+        }
+
+
+        if (aGenericPlayer.GetTotal() != 0)
+        {
+            std::cout << "(" << aGenericPlayer.GetTotal() << ")";
+        }
+    }
+    else
+    {
+        os << "<empty>";
+    }
+
+    return os;
+}
+
+
