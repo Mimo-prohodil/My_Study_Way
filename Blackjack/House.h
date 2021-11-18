edfa592c555b3ff114ec4a2d984b9b79a51f598b
Mimo-prohodil <75292060+Mimo-prohodil@users.noreply.github.com>
2021-11-19 02:30:57 +0300

    Delete Blackjack directory

--- Blackjack/House.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "GenericPlayer.h"
-class House : public GenericPlayer {
-public:
-	House(const std::string& name = "House") {};
-	virtual bool isHitting() const override;
-	void FlipFirstCard();
-};
-
